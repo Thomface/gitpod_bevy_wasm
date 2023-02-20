9b443087736b8b9ed12bb9068145f3911098a980
Thomface <thomas.boniface@gmail.com>
2023-02-20 16:03:56 +0000

    TD init

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,53 @@
-use bevy::{prelude::*};
+use bevy::prelude::*;
 
-mod plugin;
-
-use plugin::GamePlugin;
+pub const HEIGHT: f32 = 720.0;
+pub const WIDTH: f32 = 1280.0;
 
 fn main() {
+    #[cfg(target_arch = "wasm32")]
+    console_error_panic_hook::set_once();
+
     App::new()
-        .insert_resource(WindowDescriptor {
-            title: "Bevy Playground".to_string(),
-            #[cfg(target_arch = "wasm32")]
-            canvas: Some("#playground".to_string()),
+        .insert_resource(ClearColor(Color::rgb(0.4, 0.4, 0.4)))
+        .add_startup_system(spawn_basic_scene)
+        .add_startup_system(spawn_camera)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            window: WindowDescriptor {
+                #[cfg(target_arch = "wasm32")]
+                canvas: Some("#playground".to_string()),
+                width: WIDTH,
+                height: HEIGHT,
+                title: "TDD".to_string(),
+                resizable: false,
+                ..Default::default()
+            },
             ..default()
-        })
-        .add_plugins(DefaultPlugins)
-        .add_plugin(GamePlugin)
+        }))
         .run();
+}
+
+fn spawn_basic_scene(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
+        material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+        ..default()
+    });
+
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+        material: materials.add(Color::rgb(0.67, 0.84, 0.92).into()),
+        transform: Transform::from_xyz(0.0, 0.5, 0.0),
+        ..default()
+    });
+}
+
+fn spawn_camera(mut commands: Commands) {
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
 }
--- src/positioning.rs
+++
@@ -1,3 +0,0 @@
-use bevy::prelude::{Vec2, Vec3};
-
-pub const DECK_START_POS: Vec3 = Vec3::new(300.0, 0.0, 0.0);
